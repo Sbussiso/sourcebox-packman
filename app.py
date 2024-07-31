9b25c9bdcaaf50c285a7ab5f067d07c81cebb23d
sbussiso <sbussiso321@gmail.com>
2024-07-31 07:53:33 +0000

    fixed port binding

--- app.py
+++ app.py
@@ -260,4 +260,5 @@ def logout():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, use_reloader=False, host="0.0.0.0", port=port) #was port 80
